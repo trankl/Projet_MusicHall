d2775200f57f8b4b861cd3b425ffc48c7027461a
trankl <trankhanhly88@gmail.com>
2021-11-12 22:20:46 +0100

    Create new class Service and Resource for Instrument

---
+++ musichall/src/main/java/org/restapi/crud/musichall/resource/InstrumentResource.java
@@ -0,0 +1,88 @@
+package org.restapi.crud.musichall.resource;
+
+import java.util.List;
+
+import org.restapi.crud.musichall.model.Instrument;
+import org.restapi.crud.musichall.service.InstrumentService;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path ("/instrument")
+public class InstrumentResource {
+	
+		private InstrumentService service = new InstrumentService();
+
+		@GET
+	    @Produces(MediaType.TEXT_PLAIN)
+		public String getIt() {
+	        return "Got instrument!!!!";
+	    }
+	
+		//cette methode a definit la partie de l'url d'acces a webservice sous path /getAllInstruments
+		//elle s'utilise avec GET
+		//elle prend du JSON en entree
+		@Path ("/getAllInstrument")
+		@GET  
+		@Produces(MediaType.APPLICATION_JSON) 
+		public List<Instrument> getAllInstrument () throws Exception {
+			// On demande au service d'executer la methode "getAll" et retour la liste
+			return service.getAllIntrument();
+
+		}
+
+	
+		//cette methode a definit la partie de l'url d'acces a webservice sous path /insertionInstrument
+		//elle s'utilise avec POST
+		//elle prend du JSON en entree
+		@Path ("/insertionInstrument")
+		@POST  
+		@Consumes(MediaType.APPLICATION_JSON) 
+		public Instrument addInstrument (Instrument instrument) throws Exception {
+			// On demande au service d'executer la methode "insertInstrument" et retour user
+			return service.insertInstrument(instrument);
+			
+		}	
+		
+		//cette methode a definit la partie de l'url d'acces a webservice sous path /updateInstrument
+		//elle s'utilise avec POST
+		//elle prend du JSON en entree
+		@Path("/updateInstrument/{id}")
+		@POST
+		@Consumes(MediaType.APPLICATION_JSON)
+		public void updateInstrument (Instrument instrument, @PathParam("id") int id) throws Exception {
+			// On demande au service d'executer la methode "update" 
+			service.updateInstrument(instrument, id);
+		}
+		
+		//cette methode a definit la partie de l'url d'acces a webservice sous path /deleteInstrument/{id}
+		//elle s'utilise avec DELETE
+		//elle prend du JSON en entree
+		@Path ("/deleteInstrument/{id}")
+		@DELETE  
+		@Consumes(MediaType.APPLICATION_JSON) 
+		public void deleteInstrument (@PathParam("id") int id) throws Exception {
+			// On demande au service d'executer la methode "remove" 
+			service.removeInstrument(id);
+			
+		}
+		
+
+		//cette methode a definit la partie de l'url d'acces a webservice sous path //findbyIDInstrument/{id}
+		//elle s'utilise avec GET
+		//elle prend du JSON en entree
+		@Path("/findbyIDInstrument/{id}")
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public Instrument getInstrument (@PathParam("id") int id) throws Exception {
+			// On demande au service d'executer la methode "findById(id)" et retour la instrument avec id recherche
+			return service.findById(id);
+		}
+}
---
+++ musichall/src/main/java/org/restapi/crud/musichall/service/InstrumentService.java
@@ -0,0 +1,179 @@
+package org.restapi.crud.musichall.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.restapi.crud.musichall.model.Instrument;
+
+public class InstrumentService {
+	// Demarer le systeme
+	EntityManagerFactory entityManagerFactory = null;
+	EntityManager entityManager = null;
+
+	// Methode pour ajouter nouveau instrument dans database
+	public Instrument insertInstrument(Instrument instrument) throws Exception{
+
+		try {
+			// Appel fichier persistence.xml
+			entityManagerFactory = Persistence.createEntityManagerFactory("app-DB"); 
+			System.out.println("Get connect");
+			// Demander la connection au DB
+			entityManager = entityManagerFactory.createEntityManager();
+			System.out.println("Connected");
+			// Transaction des donnes -> pour Modifier les objets 
+			EntityTransaction trans = entityManager.getTransaction();
+			//commencer la transaction
+			trans.begin(); 
+			
+			// Synchronyser valeur au donnee pour ajouter nouveau instrument
+			entityManager.persist(instrument);
+
+			// finir la transaction
+			trans.commit();
+
+			// Afficher nouveau instrument
+			System.out.println("Cree nouveau "+instrument);
+
+			return instrument;
+		}
+		finally {
+			// fermer la connection au DB
+			if (entityManager!=null) { entityManager.close();}
+			if (entityManagerFactory!=null) {entityManagerFactory.close();
+			System.out.println("Disconnected");}
+		}
+
+	}
+
+
+	// Methode pour afficher list des instruments
+	public List<Instrument> getAllIntrument() throws Exception {
+		// Declarer list des variables instruments
+		List<Instrument> listInstrument = new ArrayList<>();
+		try {
+			// Appel fichier persistence.xml
+			entityManagerFactory = Persistence.createEntityManagerFactory("app-DB"); 
+			System.out.println("Get connect");
+			// Demander la connection au DB
+			entityManager = entityManagerFactory.createEntityManager();
+			System.out.println("Connected");
+
+			// Afficher message
+			System.out.println("List des instruments");
+			// Envoyer requete pour afficher et get la table de donnees (ou bien list des objets): Attention nom de table dans Querry est la Class dans JAVA
+			listInstrument = entityManager.createQuery("from Instrument", Instrument.class).getResultList();
+
+			// Afficher list des musiciens dans log console
+			for (Instrument instrument : listInstrument) {
+				System.out.println(instrument);
+			}
+
+			return listInstrument;
+		}
+		finally {
+			// fermer la connection au DB
+			if (entityManager!=null) { entityManager.close();}
+			if (entityManagerFactory!=null) {entityManagerFactory.close();
+			System.out.println("Disconnected");}
+		}
+	}
+
+
+	public void removeInstrument(int id) throws Exception{
+		try {
+			// Appel fichier persistence.xml
+			entityManagerFactory = Persistence.createEntityManagerFactory("app-DB"); // "app-DB" est nom de fichier persistence
+			System.out.println("Get connect to database...");
+			// Demander la connection au DB
+			entityManager = entityManagerFactory.createEntityManager();
+			System.out.println("Connected");
+
+			// Appel 1 seul objet au id = 1
+			Instrument instrument = entityManager.find(Instrument.class,id );
+
+			// Transaction des donnes -> pour Modifier les objets 
+			EntityTransaction trans = entityManager.getTransaction();
+			trans.begin(); //commencer la transaction
+
+			// Supprimer 1 object
+			entityManager.remove( instrument );
+			System.out.println("Supprimee " + instrument);
+
+			trans.commit();// finir la transaction
+
+		}
+		finally {
+			// fermer la connection au DB
+			if (entityManager!=null) { entityManager.close();}
+			if (entityManagerFactory!=null) {entityManagerFactory.close();
+			System.out.println("Disconnected");}
+		}
+
+	}
+
+	public void updateInstrument(Instrument instrument, int id) throws Exception {
+		try {
+			// Appel fichier persistence.xml
+			entityManagerFactory = Persistence.createEntityManagerFactory("app-DB"); // "app-DB" est nom de fichier persistence
+			System.out.println("Get connect to database...");
+			// Demander la connection au DB
+			entityManager = entityManagerFactory.createEntityManager();
+			System.out.println("Connected");
+
+			// Appel 1 seul objet au id 
+				Instrument instrumentDB = entityManager.find(Instrument.class,id);
+				System.out.println("afficher "+instrumentDB);
+
+			// Transaction des donnes -> pour Modifier les objets 
+			EntityTransaction trans = entityManager.getTransaction();
+			trans.begin(); //commencer la transaction
+
+			// Modifier object
+			instrumentDB.setInstrument_nom(instrument.getInstrument_nom());
+			instrumentDB.setInstrument_masque(instrument.getInstrument_masque());
+			
+			// Synchronyser valeur au donnee 
+			entityManager.persist(instrumentDB);
+			System.out.println("Modifier instrument id: " +id+" est "+ instrumentDB);
+			
+			// finir la transaction
+			trans.commit();
+	}
+		finally {
+			// fermer la connection au DB
+			if (entityManager!=null) { entityManager.close();}
+			if (entityManagerFactory!=null) {entityManagerFactory.close();
+			System.out.println("Disconnected");}
+		}
+	}
+
+	public Instrument findById(int id) throws Exception {
+		try {
+			// Appel fichier persistence.xml
+			entityManagerFactory = Persistence.createEntityManagerFactory("app-DB"); // "app-DB" est nom de fichier persistence
+			System.out.println("Get connect to database...");
+			// Demander la connection au DB
+			entityManager = entityManagerFactory.createEntityManager();
+			System.out.println("Connected");
+
+			// Appel 1 seul objet au id
+			Instrument instrument = entityManager.find(Instrument.class,id);
+			System.out.println("afficher instrument au id "+id+" est "+instrument);
+			
+			return instrument;
+	}
+		finally {
+			// fermer la connection au DB
+			if (entityManager!=null) { entityManager.close();}
+			if (entityManagerFactory!=null) {entityManagerFactory.close();
+			System.out.println("Disconnected");}
+		}
+		
+	}
+
+}
